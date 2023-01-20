a76b32dde6c9352e89598bc7cbb59b4c77abce5e
ivamtra <97352752+ivamtra@users.noreply.github.com>
2023-01-20 15:40:23 +0000

    Filereader nu úreldur

--- src/filebuilder/filebuilder.js
+++ src/filebuilder/filebuilder.js
@@ -1,39 +1,58 @@
 /* eslint-disable no-unused-vars */
 import * as fs from 'fs'
-import { readCSVFile, readJSONFile } from '../filereader/filereader.js'
 import { csvToArray, getPageName } from '../lib/lib.js'
 import { createHomePage, createPage } from '../uiBuilder/uiBuilder.js'
 // eslint-disable-next-line no-unused-vars
 
-// String path => String html
-export function buildPage(csvFile) {
-    const lastSlash = csvFile.lastIndexOf('/')
+
+
+export function buildPage2(csvFile) {
+  const lastSlash = csvFile.lastIndexOf('/')
     // Eitthvað.html
-    const fileName = `../../dist/${csvFile.substring(lastSlash+1, csvFile.length-4)  }.html`
-    readCSVFile(csvFile, (data) => {
-        const arr = csvToArray(data)
+  const fileName = `../../dist/${csvFile.substring(lastSlash+1, csvFile.length-4)  }.html`
+  fs.readFile(csvFile, 'latin1', (err, data) => {
+    if (err) throw err
+    
+    const arr = csvToArray(data)
         // Athuga hvort að fylkið uppfylli skilyrðin
         if (!arr || arr.length <= 2) return
         const html = createPage(getPageName(fileName),arr)
-        fs.writeFileSync(fileName, html, (err, _data) => {
-          if (err) throw err
+        fs.writeFileSync(fileName, html, (err2, _data) => {
+          if (err2) throw err2
         })
-    })
+  })
+
 }
 
+// export function buildHomePage(jsonFile) {
+//   const lastSlash = jsonFile.lastIndexOf('/')
+//   // Eitthvað.html
+//   const fileName = `../../dist/${jsonFile.substring(lastSlash+1, jsonFile.length-5)  }.html`
+//   readJSONFile(jsonFile, jsonString => {
+//     const obj = JSON.parse(jsonString)
+//     const html = createHomePage('Heimasíða', obj)
+//     fs.writeFileSync(fileName, html, (err, _data) => {
+//       if (err) throw err
+//     })
 
-export function buildHomePage(jsonFile) {
+//   })
+
+ export function buildHomePage2(jsonFile) {
   const lastSlash = jsonFile.lastIndexOf('/')
   // Eitthvað.html
   const fileName = `../../dist/${jsonFile.substring(lastSlash+1, jsonFile.length-5)  }.html`
-  readJSONFile(jsonFile, jsonString => {
+  fs.readFile(jsonFile, (err, data) => {
+    if (err) throw err;
     const obj = JSON.parse(jsonString)
     const html = createHomePage('Heimasíða', obj)
-    fs.writeFileSync(fileName, html, (err, _data) => {
-      if (err) throw err
+    fs.writeFileSync(fileName, html, (err2, _data) => {
+      if (err2) throw err2
     })
-
   })
+
+ } 
+
+  
 
 
 }
--- src/main/main.js
+++ src/main/main.js
@@ -1,4 +1,4 @@
-import { buildHomePage, buildPage } from '../filebuilder/filebuilder.js';
+import { buildHomePage2, buildPage2 } from '../filebuilder/filebuilder.js';
 import { readDirectory } from '../filereader/filereader.js';
 import { getFileExtension } from '../lib/lib.js';
 
@@ -6,10 +6,10 @@ const PATH = '../data'
 readDirectory(PATH, files => {
     files.forEach(file => {
         if (getFileExtension(file) === '.csv') {
-            buildPage(`${PATH  }/${file}`)
+            buildPage2(`${PATH  }/${file}`)
         }
         else if (getFileExtension(file) === '.json') {
-            buildHomePage(`${PATH  }/${file}`)
+            buildHomePage2(`${PATH  }/${file}`)
         }
     });
 })
