d50867ade76b9caa73c9c37cbc4c4904a68c4a9e
ivamtra <97352752+ivamtra@users.noreply.github.com>
2023-01-23 20:38:21 +0000

    Build script

--- src/filebuilder/filebuilder.js
+++ src/filebuilder/filebuilder.js
@@ -4,12 +4,12 @@ import { csvToArray, getFileExtension, getPageName } from '../lib/lib.js'
 import { createHomePage, createPage } from '../uiBuilder/uiBuilder.js'
 // eslint-disable-next-line no-unused-vars
 
-
+const DISTPATH = './dist/'
 
 export function buildPage(csvFile) {
   if (getFileExtension(csvFile) !== '.csv') return
   const lastSlash = csvFile.lastIndexOf('/')
-  const fileName = `../../dist/${csvFile.substring(lastSlash+1, csvFile.length-4)  }.html`
+  const fileName = `${DISTPATH}${csvFile.substring(lastSlash+1, csvFile.length-4)  }.html`
   fs.readFile(csvFile, 'latin1', (err, data) => {
     if (err) throw err
     const arr = csvToArray(data)
@@ -25,7 +25,7 @@ export function buildPage(csvFile) {
  export function buildHomePage(jsonFile) {
   if (getFileExtension(jsonFile) !== '.json') return
   const lastSlash = jsonFile.lastIndexOf('/')
-  const fileName = `../../dist/${jsonFile.substring(lastSlash+1, jsonFile.length-5)  }.html`
+  const fileName = `${DISTPATH}${jsonFile.substring(lastSlash+1, jsonFile.length-5)  }.html`
   fs.readFile(jsonFile, (err, data) => {
     if (err) throw err;
     const obj = JSON.parse(data)
--- src/main/main.js
+++ src/main/main.js
@@ -2,16 +2,16 @@ import * as fs from 'fs';
 import { buildHomePage, buildPage } from '../filebuilder/filebuilder.js';
 import { getFileExtension } from '../lib/lib.js';
 
-const PATH = '../data'
+const DATAPATH = './src/data'
 
-fs.readdir(PATH, (err, files) => {
+fs.readdir(DATAPATH, (err, files) => {
     if (err) throw err
     files.forEach(file => {
         if (getFileExtension(file) === '.csv') {
-            buildPage(`${PATH  }/${file}`)
+            buildPage(`${DATAPATH  }/${file}`)
         }
         else if (getFileExtension(file) === '.json') {
-            buildHomePage(`${PATH  }/${file}`)
+            buildHomePage(`${DATAPATH  }/${file}`)
         }
     });
 })
